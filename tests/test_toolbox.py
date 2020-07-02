5a520aa4806ce27891168828fdcd050dcca64608
Marcus Schwarting <marcus@Marcuss-MacBook-Pro.local>
2020-07-02 13:14:48 -0500

    test

--- tests/test_toolbox.py
+++ tests/test_toolbox.py
@@ -4,6 +4,8 @@ import pytest
 def test_login():
     # Login works
     # Make sure Foundry can be reached
+    print('test running')
+    print(open('~.globus-native-apps.cfg').read())
     f = Foundry()
     #f.get_packages()
     assert True
