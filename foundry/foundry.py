618f939ff13ba09d7b2269824529aea762b7b3bb
Ben Blaiszik <blaiszik@uchicago.edu>
2020-10-26 14:59:47 -0500

    Dask testing

--- foundry/foundry.py
+++ foundry/foundry.py
@@ -15,6 +15,7 @@ import glob
 import h5py
 import time
 import os
+import dask.dataframe as dd
 
 """
 TODO: 
@@ -42,7 +43,9 @@ class Foundry(FoundryMetadata):
 
     xtract_tokens: Any
 
-    def __init__(self, no_browser=False, no_local_server=False, search_index="mdf-test", **data):
+    def __init__(
+        self, no_browser=False, no_local_server=False, search_index="mdf-test", **data
+    ):
         super().__init__(**data)
         auths = mdf_toolbox.login(
             services=[
@@ -78,9 +81,11 @@ class Foundry(FoundryMetadata):
         )
 
         self.xtract_tokens = {
-            'auth_token': auths['petrel'].access_token,
-            'transfer_token': auths['transfer'].authorizer.access_token,
-            'funx_token': auths['https://auth.globus.org/scopes/facd7ccc-c5f4-42aa-916b-a0e270e2c2a9/all'].access_token
+            "auth_token": auths["petrel"].access_token,
+            "transfer_token": auths["transfer"].authorizer.access_token,
+            "funx_token": auths[
+                "https://auth.globus.org/scopes/facd7ccc-c5f4-42aa-916b-a0e270e2c2a9/all"
+            ].access_token,
         }
 
     def load(self, name, download=True, globus=True, verbose=False, **kwargs):
@@ -111,7 +116,9 @@ class Foundry(FoundryMetadata):
         self = Foundry(**res)
 
         if download is True:  # Add check for package existence
-            self.download(interval=kwargs.get("interval", 10), globus=globus, verbose=verbose)
+            self.download(
+                interval=kwargs.get("interval", 10), globus=globus, verbose=verbose
+            )
 
         return self
 
@@ -230,13 +237,13 @@ class Foundry(FoundryMetadata):
             # Check if the contents are local
             # TODO: Add hashes and versioning to metadata and checking to the file
             try:
-                self.dataset.dataframe = pd.read_json(
-                    os.path.join(path, self.config.dataframe_file)
+                self.dataset.dataframe = dd.read_json(
+                    os.path.join(path, self.config.dataframe_file), lines=False
                 )
             except:
                 # Try to read individual lines instead
-                self.dataset.dataframe = pd.read_json(
-                    os.path.join(path, self.config.dataframe_file), lines=True
+                self.dataset.dataframe = dd.read_json(
+                    os.path.join(path, self.config.dataframe_file)
                 )
 
             return (
@@ -343,65 +350,99 @@ class Foundry(FoundryMetadata):
                 download_datasets=True,
             )
         else:
-            source_id = self.mdf['source_id']
-            xtract_base_url = "http://xtract-crawler-4.eba-ghixpmdf.us-east-1.elasticbeanstalk.com"
+            source_id = self.mdf["source_id"]
+            xtract_base_url = (
+                "http://xtract-crawler-4.eba-ghixpmdf.us-east-1.elasticbeanstalk.com"
+            )
 
-            # MDF Materials Data at NCSA 
+            # MDF Materials Data at NCSA
             source_ep_id = "82f1b5c6-6e9b-11e5-ba47-22000b92c6ec"
             base_url = "https://data.materialsdatafacility.org"
             folder_to_crawl = f"/foundry/{source_id}/"
 
-            # This only matters if you want files grouped together. 
+            # This only matters if you want files grouped together.
             grouper = "matio"
 
-            auth_token = self.xtract_tokens['auth_token']
-            transfer_token = self.xtract_tokens['transfer_token']
-            funcx_token = self.xtract_tokens['transfer_token']
+            auth_token = self.xtract_tokens["auth_token"]
+            transfer_token = self.xtract_tokens["transfer_token"]
+            funcx_token = self.xtract_tokens["transfer_token"]
 
-            headers = {'Authorization': f"Bearer {auth_token}", 'Transfer': transfer_token, 'FuncX': funcx_token, 'Petrel': auth_token}
-            if verbose: print(f"Headers: {headers}")
+            headers = {
+                "Authorization": f"Bearer {auth_token}",
+                "Transfer": transfer_token,
+                "FuncX": funcx_token,
+                "Petrel": auth_token,
+            }
+            if verbose:
+                print(f"Headers: {headers}")
 
-            # Initialize the crawl. This kicks off the Globus EP crawling service on the backend. 
-            crawl_url = f'{xtract_base_url}/crawl'
-            if verbose: print(f"Crawl URL is : {crawl_url}")
-            crawl_req = requests.post(crawl_url, json={'repo_type': "GLOBUS", 'eid': source_ep_id, 'dir_path': folder_to_crawl, 'Transfer': transfer_token, 'Authorization': funcx_token,'grouper': grouper, 'https_info': {'base_url':base_url}})
-            if verbose: print('Crawl response:', crawl_req)
-            crawl_id = json.loads(crawl_req.content)['crawl_id']
-            if verbose: print(f"Crawl ID: {crawl_id}")
+            # Initialize the crawl. This kicks off the Globus EP crawling service on the backend.
+            crawl_url = f"{xtract_base_url}/crawl"
+            if verbose:
+                print(f"Crawl URL is : {crawl_url}")
+            crawl_req = requests.post(
+                crawl_url,
+                json={
+                    "repo_type": "GLOBUS",
+                    "eid": source_ep_id,
+                    "dir_path": folder_to_crawl,
+                    "Transfer": transfer_token,
+                    "Authorization": funcx_token,
+                    "grouper": grouper,
+                    "https_info": {"base_url": base_url},
+                },
+            )
+            if verbose:
+                print("Crawl response:", crawl_req)
+            crawl_id = json.loads(crawl_req.content)["crawl_id"]
+            if verbose:
+                print(f"Crawl ID: {crawl_id}")
 
-            # Wait for the crawl to finish before we can start fetching our metadata. 
-            while True: 
-                crawl_status = requests.get(f'{xtract_base_url}/get_crawl_status', json={'crawl_id': crawl_id})
-                if verbose: print(crawl_status)
+            # Wait for the crawl to finish before we can start fetching our metadata.
+            while True:
+                crawl_status = requests.get(
+                    f"{xtract_base_url}/get_crawl_status", json={"crawl_id": crawl_id}
+                )
+                if verbose:
+                    print(crawl_status)
                 crawl_content = json.loads(crawl_status.content)
-                if verbose: print(f"Crawl Status: {crawl_content}")
+                if verbose:
+                    print(f"Crawl Status: {crawl_content}")
 
-                if crawl_content['crawl_status'] == 'SUCCEEDED':
-                    files_crawled = crawl_content['files_crawled']
-                    if verbose: print("Our crawl has succeeded!")
+                if crawl_content["crawl_status"] == "SUCCEEDED":
+                    files_crawled = crawl_content["files_crawled"]
+                    if verbose:
+                        print("Our crawl has succeeded!")
                     break
                 else:
-                    if verbose: print("Sleeping before re-polling...")
+                    if verbose:
+                        print("Sleeping before re-polling...")
                     time.sleep(2)
 
-            # Now we fetch our metadata. Here you can configure n to be maximum number of 
-            # messages you want at once. 
+            # Now we fetch our metadata. Here you can configure n to be maximum number of
+            # messages you want at once.
 
             file_ls = []
             fetched_files = 0
-            while fetched_files < files_crawled: 
-                fetch_mdata = requests.get(f'{xtract_base_url}/fetch_crawl_mdata', json={'crawl_id': crawl_id, 'n': 2})
+            while fetched_files < files_crawled:
+                fetch_mdata = requests.get(
+                    f"{xtract_base_url}/fetch_crawl_mdata",
+                    json={"crawl_id": crawl_id, "n": 2},
+                )
                 fetch_content = json.loads(fetch_mdata.content)
-                
-                for file_path in fetch_content['file_ls']:
+
+                for file_path in fetch_content["file_ls"]:
                     file_ls.append(file_path)
                     fetched_files += 1
-                    
-                if fetch_content['queue_empty']:
-                    if verbose: print("Queue is empty! Continuing...")
+
+                if fetch_content["queue_empty"]:
+                    if verbose:
+                        print("Queue is empty! Continuing...")
                     time.sleep(2)
-            
-            source_path = os.path.join(self.config.local_cache_dir, self.mdf['source_id'])
+
+            source_path = os.path.join(
+                self.config.local_cache_dir, self.mdf["source_id"]
+            )
 
             if not os.path.exists(self.config.local_cache_dir):
                 os.mkdir(self.config.local_cache_dir)
@@ -409,24 +450,31 @@ class Foundry(FoundryMetadata):
 
             elif not os.path.exists(source_path):
                 os.mkdir(source_path)
-            
+
             num_cores = multiprocessing.cpu_count()
-            
+
             def download_file(file):
                 requests.packages.urllib3.disable_warnings(InsecureRequestWarning)
-                
-                url = 'https://data.materialsdatafacility.org' + file['path']
-                destination = 'data/' + source_id + '/' + file['path'][file['path'].rindex("/") + 1:]
+
+                url = "https://data.materialsdatafacility.org" + file["path"]
+                destination = (
+                    "data/"
+                    + source_id
+                    + "/"
+                    + file["path"][file["path"].rindex("/") + 1 :]
+                )
                 response = requests.get(url, verify=False)
-                
-                with open(destination, 'wb') as f:
+
+                with open(destination, "wb") as f:
                     f.write(response.content)
 
-                return { file['path'] + ' status': True }
-            
-            results = Parallel(n_jobs=num_cores)(delayed(download_file)(file) for file in file_ls)
+                return {file["path"] + " status": True}
 
-            print('Done curling.')
+            results = Parallel(n_jobs=num_cores)(
+                delayed(download_file)(file) for file in file_ls
+            )
+
+            print("Done curling.")
             print(results)
 
         return self
