288e28fffe90ba450227576c311429ef0e2c1deb
ribhavb <68833659+ribhavb@users.noreply.github.com>
2021-07-15 16:43:32 -0400

    authorizer thing

--- foundry/foundry.py
+++ foundry/foundry.py
@@ -47,28 +47,32 @@ class Foundry(FoundryMetadata):
     xtract_tokens: Any
 
     def __init__(
-        self, no_browser=False, no_local_server=False, search_index="mdf-test", **data
+        self, no_browser=False, no_local_server=False, index="mdf", authorizers=None, **data
     ):
         super().__init__(**data)
-        auths = mdf_toolbox.login(
-            services=[
-                "data_mdf",
-                "mdf_connect",
-                "search",
-                "petrel",
-                "transfer",
-                "dlhub",
-                "openid",
-                "https://auth.globus.org/scopes/facd7ccc-c5f4-42aa-916b-a0e270e2c2a9/all",
-            ],
-            app_name="Foundry",
-            make_clients=True,
-            no_browser=no_browser,
-            no_local_server=no_local_server,
-        )
+
+        if authorizers:
+            auths = authorizers
+        else:
+            auths = mdf_toolbox.login(
+                services=[
+                    "data_mdf",
+                    "mdf_connect",
+                    "search",
+                    "petrel",
+                    "transfer",
+                    "dlhub",
+                    "openid",
+                    "https://auth.globus.org/scopes/facd7ccc-c5f4-42aa-916b-a0e270e2c2a9/all",
+                ],
+                app_name="Foundry",
+                make_clients=True,
+                no_browser=no_browser,
+                no_local_server=no_local_server,
+            )
 
         self.forge_client = Forge(
-            index=search_index,
+            index=index,
             services=None,
             search_client=auths["search"],
             transfer_client=auths["transfer"],
@@ -76,9 +80,13 @@ class Foundry(FoundryMetadata):
             petrel_authorizer=auths["petrel"],
         )
 
+        if index == "mdf":
+            test = False
+        else:
+            test = True
         # TODO: when release-ready, remove test=True
         self.connect_client = MDFConnectClient(
-            authorizer=auths["mdf_connect"], test=True
+            authorizer=auths["mdf_connect"], test=test
         )
 
         self.dlhub_client = DLHubClient(
