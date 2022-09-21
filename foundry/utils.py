2ca3574a4992f751e434a8504427fa713b21a8d9
Ben Blaiszik <blaiszik@uchicago.edu>
2022-09-21 14:42:48 -0500

    Moving some functions to utils

---
+++ foundry/utils.py
@@ -0,0 +1,14 @@
+import pandas as pd
+
+def is_pandas_pytable(group):
+    if 'axis0' in group.keys() and 'axis1' in group.keys():
+        return True
+    else:
+        return False
+
+
+def is_doi(string: str):
+    if string.startswith('10.') or string.startswith('https://doi.org/'):
+        return True
+    else:
+        return False
