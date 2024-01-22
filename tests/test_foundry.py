afdb2b962e5daf68c270072b486edb737b8e549c
Steven Wangen <i.blue442@gmail.com>
2024-01-22 14:01:39 -0600

    skipping problematic test in GHA

--- tests/test_foundry.py
+++ tests/test_foundry.py
@@ -303,8 +303,7 @@ def test_search_limit():
     assert dataset.dc.get("publicationYear", None) is not None
 
 
-@pytest.mark.skip()
-# @pytest.mark.skipif(bool(is_gha), reason='Checking if tests work #5')
+@pytest.mark.skipif(bool(is_gha), reason='Checking if tests work #5')
 def test_search_no_results():
     f = foundry.Foundry()
 
