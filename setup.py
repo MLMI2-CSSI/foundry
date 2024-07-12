dfeb7fe7f778e1e954aaf23741157cabd04fcaf0
Ben Blaiszik <blaiszik@uchicago.edu>
2024-07-12 14:16:18 -0500

    Update requirements to line up with Garden Pydantic requirements

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ setuptools.setup(
         "dlhub_sdk>=1.0.0",
         "numpy>=1.15.4",
         "pandas>=0.23.4",
-        "pydantic>=1.4",
+        "pydantic>=2.7.2",
         "mdf_connect_client>=0.4.0",
         "h5py>=2.10.0",
         "json2table"
